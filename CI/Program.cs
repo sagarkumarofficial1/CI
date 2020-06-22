c2f90b9e6d02200592aa975e6ac0862b80ae099c
Sagar Kumar <49971003+sagarkumarofficial1@users.noreply.github.com>
2020-06-22 18:48:08 +0530

    Update Program.cs

--- CI/Program.cs
+++ CI/Program.cs
@@ -6,7 +6,7 @@ namespace CI
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!")
+            Console.WriteLine("Hello World!");
         }
     }
 }
